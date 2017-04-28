0ae79d84c9152f8b7e3bd7787893be36f05ca463
hufan-Akari <hufan.akarin@Gmail.com>
2017-04-28 13:02:28 +0800

    use gh-pages subtree

---
+++ sw.js
@@ -0,0 +1,37 @@
+const cacheName = "akari-lrc-maker-2017.05.01-2";
+const urlsToCache = [
+  "./",
+  "dist/app.css",
+  "dist/React.js",
+  "dist/ReactDOM.js",
+  "dist/app.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      ))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
